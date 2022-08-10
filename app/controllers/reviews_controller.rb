00d2f1a36af24e2ec06ef6863d15925bd4bead9d
Yasmin Jones Sheahan <y.jsheahan@gmail.com>
2022-08-10 22:43:36 +0100

    created review form and create method to save

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,26 @@ class ReviewsController < ApplicationController
 
     def index
 
-        @reviews = ["Little Women", "Sylvia", "The Power of Now"]
+        @reviews = Review.all
 
     end
 
     def new
 
         @review = Review.new
+
+    end
+
+    def create
+
+        # take info from form and add it to the DB
+        @review = Review.new(params.require(:review).permit(:title, :body, :score, :author, :genre))
+
+        # save this to the DB
+        @review.save
+
+        # redirect back to the homepagee
+        redirect_to root_path
 
     end
     
