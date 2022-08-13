3557bf1d0a5f47e07b05451f72929580c9d42a26
Yasmin Jones Sheahan <y.jsheahan@gmail.com>
2022-08-13 09:29:39 +0100

    urls made more seo-friendly

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,10 @@ class Review < ApplicationRecord
     validates :score, numericality: { only_integer: true , greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
     validates :author, presence: true
     validates :genre, presence: true
+
+    def to_param
+        # make url friendly
+        id.to_s + "-" + title.parameterize
+    end
+    
 end
